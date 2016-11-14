6443a2ec95f19ca3efd2d7f0094fe2b4a18e6235
Mr. MOK Tsz Fung <tszfmok@ad.cityu.edu.hk>
2016-11-14 17:48:00 +0800

    change to doraemons

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,35 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.controller('MyCtrl', ['$scope','$window', function($scope,$window) {
+  $scope.groups = [];
+  for (var i=0; i<10; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: []
+    };
+    for (var j=0; j<3; j++) {
+      $scope.groups[i].items.push('Item--**' + i + '-' + j);
+    }
+  }
+
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+
+  $scope.go = function ( path ) {
+    $window.location.href = path;
+  };
+
+}]);
